c71797e0d9e71ba42a80b2f63f3404c9a6cf1acd
Kaingusaid <kaingusaid22@gmail.com>
2022-03-10 16:26:18 -0800

    where

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - entry point
+ *main - Entry point
  *my hello. printf task
  *Return: return 1 after program execution
  */
 int main(void)
-{	
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }

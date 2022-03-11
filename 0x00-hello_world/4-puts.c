201f1c95d991d8af829c34d8772c05da1e5f992f
Kaingusaid <kaingusaid22@gmail.com>
2022-03-10 17:10:24 -0800

    elsewhere

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - entry point
+ *main - entry point
  *
- * Return: 0 after printing the function
+ *Return: 0 after printing the function
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle"):
-	return (0);
+puts("/ "Programming is like building a multilingual puzzle");
+return (0);
 }
-
+ 

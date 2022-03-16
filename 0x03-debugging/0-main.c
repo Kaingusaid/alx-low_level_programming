53229bfac32c678f2391a2bf018c5f9e7379f864
Kaingusaid <kaingusaid22@gmail.com>
2022-03-16 14:19:05 -0700

    my commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,17 @@
 #include "main.h"
-
 /**
-  * main - Test function for positive or negative
-  * Return: 0
-  */
+ * main - main
+ * Return: 0
+ */
 
 int main(void)
 {
-	int i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
+i = 0;
 
-return (0)
+positive_or_negative(i);
+
+return (0);
+
 }

a91e277407dc65a8381f1f52655eddf25f1fc41f
Kaingusaid <kaingusaid22@gmail.com>
2022-03-30 08:40:33 -0700

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,36 +1,20 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * wildcmp - is a fuction that compares two strings.
- *
- * @s1: is a pointer to char.
- * @s2: is a pointer to char.
- *
- * Return: Returns the transformed pointer.
- *
- **/
-
+ * wildcmp - compares two strings.
+ * @s1: string 1.
+ * @s2: string 2. It can contains a * as a special character.
+ * Return: 1 if are identical, 0 if not.
+ */
 int wildcmp(char *s1, char *s2)
 {
-if (*s1 == '\0' && *s2 == '\0')
-{
-return (1);
-}
-else if (*s1 == *s2)
-{
-return (wildcmp(s1 + 1, s2 + 1));
-}
-if (*s2 == '*')
-{
-if (*(s2 + 1) == '*')
-{
-return (wildcmp(s1, s2 + 1));
-}
-else if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
-{
-return (1);
-}
-}
-return (0);
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
 }

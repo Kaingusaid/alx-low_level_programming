f21b62c4588a9728f0d5b97accb80e286380fe4c
Kaingusaid <kaingusaid22@gmail.com>
2022-04-07 19:51:49 -0700

    whoa

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,11 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
-
 /**
  * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
- *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *s)
@@ -24,7 +22,6 @@ int is_digit(char *s)
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
- *
  * Return: the length of the string
  */
 int _strlen(char *s)
@@ -32,9 +29,8 @@ int _strlen(char *s)
 	int i = 0;
 
 	while (s[i] != '\0')
-	{
 		i++;
-	}
+
 	return (i);
 }
 
@@ -51,7 +47,6 @@ void errors(void)
  * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- *
  * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])

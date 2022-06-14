02af2f4adf916ad66d3702b5076704f240cca808
Kaingusaid <kaingusaid22@gmail.com>
2022-06-13 17:38:47 -0700

    isalpha

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ *_isalpha - display 1 to lower/upper case, 0 otherwise
+ *@c: parameter character or ascii code
+ *Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	int result;
+
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		result = 1;
+	}
+	else
+	{
+		result = 0;
+	}
+
+	return (result);
+}
